89596e0ba16600c2ea4fa5896587da734ba63574
Guilherme <guilherme.antonio@digicade.com.br>
2021-07-19 13:41:03 -0300

    Criação tela de Login.vue

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -3,7 +3,7 @@
     <q-header elevated>
       <q-toolbar>
         <q-toolbar-title class="text-center">
-          PokeDex
+          Estudos Vue JS com Quasar
         </q-toolbar-title>
 
       </q-toolbar>
--- src/router/routes.js
+++ src/router/routes.js
@@ -4,7 +4,15 @@ const routes = [
     path: '/',
     component: () => import('layouts/MainLayout.vue'),
     children: [
-      { path: '', component: () => import('pages/Index.vue') }
+      { 
+        path: '/Index', 
+        component: () => import('pages/Index.vue') 
+      },
+      { 
+        path: '/Login', 
+        name: "Login",
+        component: () => import('pages/Login.vue') 
+      }
     ]
   },
 
